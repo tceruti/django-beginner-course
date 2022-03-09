8b61a89bb78a504cf703faa21418724f1d94f273
Tomas Ceruti <tomas.ceruti@gmail.com>
2022-03-09 19:44:36 -0300

    Add viewsets to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from profiles_api import serializers
 
 
@@ -40,3 +42,38 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    def list(self, request):
+        a_viewset = [
+            'Uses action (list, create, ...)',
+            'automatically maps to url using routers',
+            'provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'get'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'put'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'patch'})
+
+    def destroy (self, request, pk=None):
+        return Response({'http_method': 'delete'})
